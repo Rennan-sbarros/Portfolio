ca34923f127984218014f5188aae14f0a183dc07
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-05-17 20:47:27 -0300

    feat: criação do home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/core/header";
+import { Home } from "./components/pages/home";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
