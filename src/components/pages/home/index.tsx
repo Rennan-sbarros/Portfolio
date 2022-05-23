7e4aeedfa5c0641f3990d1e58ca6a85e06f0e1ac
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-05-23 18:06:49 -0300

    feat: alterando layout da home

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,69 +1,81 @@
-import imagemPrincipal from '../../../assets/images/Design-principal.png';
+import imagemPrincipal from '../../../assets/images/photo_programming.svg';
 import { FaLinkedinIn, FaGithub, FaTelegram, FaWhatsapp, FaInstagram } from 'react-icons/fa';
 
 export function Home() {
     return (
         <section className="container_geral">
 
-            <div className="conteudo">
-                <div className="card_conteudo">
-                <div className="imagem_perfil">
-                    <img src={imagemPrincipal} alt="" className="img" />
-                </div>
+            <div className="container">
+                <div className="row align-items-center content">
+                    
+                    <div className="col-md-6">
+                        <div className="card_text">
+                            <h2>Olá, eu sou</h2>
+                            <h1>Rennan Cândido</h1>
+                            <h2>Desenvolvedor Front-end</h2>
+                        </div>
+                    
+                        <ul className="redes_sociais">
+                            <li>
+                            <a
+                                href="https://www.linkedin.com/in/rennan-candido1/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaLinkedinIn/>
+                            </a>
+                            </li>
+                            <li>
+                            <a
+                                href="https://github.com/rennan-sbarros"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaGithub/>
+                            </a>
+                            </li>
+                            <li>
+                            <a
+                                href="https://t.me/Rennancandido"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaTelegram/>
+                            </a>
+                            </li>
+                            <li>
+                            <a
+                                href="https://api.whatsapp.com/send?phone=5581993880394&text=Ol%C3%A1%2C%20Rennan!%20Vim%20atrav%C3%A9s%20do%20seu%20Portf%C3%B3lio."
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaWhatsapp/>
+                            </a>
+                            </li>
+                            <li>
+                            <a
+                                href="https://www.instagram.com/rennan_candid0/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaInstagram/>
+                            </a>
+                            </li>
+                        </ul>
+                    </div>
 
-                <div className="card_text">
-                    <h1>Rennan Cândido</h1>
-                    <h2>Desenvolvedor Front-end</h2>
-                </div>
+                    <div className="col-md-6 pt-5">
+                    <img
 
-                <ul className="redes_sociais">
-                    <li>
-                    <a
-                        href="https://www.linkedin.com/in/rennan-candido1/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaLinkedinIn/>
-                    </a>
-                    </li>
-                    <li>
-                    <a
-                        href="https://github.com/rennan-sbarros"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaGithub/>
-                    </a>
-                    </li>
-                    <li>
-                    <a
-                        href="https://t.me/Rennancandido"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaTelegram/>
-                    </a>
-                    </li>
-                    <li>
-                    <a
-                        href="https://api.whatsapp.com/send?phone=5581993880394&text=Ol%C3%A1%2C%20Rennan!%20Vim%20atrav%C3%A9s%20do%20seu%20Portf%C3%B3lio."
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaWhatsapp/>
-                    </a>
-                    </li>
-                    <li>
-                    <a
-                        href="https://www.instagram.com/rennan_candid0/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaInstagram/>
-                    </a>
-                    </li>
-                </ul>
+                                src={imagemPrincipal}
+                                alt="Imagem de um grupo de cachorros"
+                            >
+                        </img>
+                    </div>
+
                 </div>
+               
+
             </div>
 
         </section>
