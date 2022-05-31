9fb7cf9496b80d14f427c459cd9ee79a70e77a9c
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-05-31 00:39:04 -0300

    feat: corrigindo Navbar Toggle

--- src/components/core/header/index.tsx
+++ src/components/core/header/index.tsx
@@ -1,62 +1,59 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { Container, Nav, Navbar} from "react-bootstrap";
 import { Link } from "react-scroll";
+import { FaBars } from "react-icons/fa";
 
 export function Header() {
     
     return (
-        <header id="home">
-            <nav className="navbar navbar-expand-lg fixed-top">
-                <div className="container-fluid">
-
-                    <a className="navbar-brand" >Rennan</a>
+            <Navbar className="navbar fixed-top" expand="lg">
+                <Container className="container-fluid">
+                    <Navbar.Brand d-lg-block className="d-none d-lg-block">Rennan</Navbar.Brand>
                     
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <Navbar.Toggle>
+                        <FaBars/>
+                    </Navbar.Toggle>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent" >
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            {/* <a  className="nav-link" href="#">Sobre mim</a> */}
-                            <Link
-                                className="nav-link"
-                                activeClass="active"
-                                to="about"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={500}
-                            > Sobre mim </Link>
-                        </li>
-                        <li className="nav-item">
-                            {/* <a className="nav-link" href="#">Habilidades</a> */}
-                            <Link
-                                className="nav-link"
-                                activeClass="active"
-                                to="skills"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={500}
-                            > Habilidades </Link>
-                        </li>
-                        <li className="nav-item">
-                            {/* <a id="sobremim" className="nav-link" href="#">Projetos</a> */}
-                            <Link
-                                className="nav-link"
-                                activeClass="active"
-                                to="projects"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={500}
-                            > Projetos </Link>
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-            </nav>
-
-        </header>
+                    <Navbar.Collapse className="navbar-collapse" id="basic-navbar-nav">
+                        <Nav className="navbar-nav ms-auto mb-2 mb-lg-0" >
+                            <Nav.Link className="nav-item">
+                                <Link
+                                    className="nav-link"
+                                    activeClass="active"
+                                    to="about"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={500}
+                                > Sobre mim </Link>
+                            </Nav.Link>
+                            <Nav.Link className="nav-item">
+                                <Link
+                                    className="nav-link"
+                                    activeClass="active"
+                                    to="skills"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={500}
+                                > Habilidades </Link>
+                            </Nav.Link>
+                            <Nav.Link className="nav-item">
+                                <Link
+                                    className="nav-link"
+                                    activeClass="active"
+                                    to="projects"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={500}
+                                > Projetos </Link>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+       
+        
     ) 
 }
