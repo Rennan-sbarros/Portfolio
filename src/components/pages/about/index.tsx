5c673c96e027f7c1ec2f86dd2ee0e18f287e819a
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-05-31 22:30:39 -0300

    feat: reformulando pagina about

--- src/components/pages/about/index.tsx
+++ src/components/pages/about/index.tsx
@@ -1,39 +1,33 @@
-import imagem_perfil from "../../../assets/images/imagem_perfil.svg";
+import imagem_about from "../../../assets/images/photo_programming_about.svg";
 
 export function About() {
     return (
-        <section className="">
-            <div className="content-about" id="about">
-                <div className="container card card-main">
-                    <div className="row align-items-center">
-                        
-                        <div className="col-md-4 d-none d-lg-block">
-                            <img src={imagem_perfil} alt="Foto de perfil de Rennan" />
-                        </div>
-
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h1 className="title text-start">Sobre mim</h1>
-                                <p className="card-text text-start"> <span>Olá mundo! Sou o Rennan. </span> 
-                                    Graduado em Análise e Desenvolvimento de Sistemas, curso em parceria com o Porto Digital,
-                                    pela Universidade Tiradentes/PE. E estagiário na Autobem Brasil como desenvolvedor Front-End.
-                                    Desta forma, me dedico a aprofundar os estudos em JavaScript com o framework Angular. Além de, possuir conhecimentos em Java e SpringBoot,
-                                    para a construção de aplicações web.
-                                </p>
-                            </div>
+        <section id='about'>
+            <div className="container container-about">
+                <h1 className="title text-center">Sobre mim</h1>
+                <div className="row align-items-center content-about">
+                    
+                    <div className="col-md-6">
+                        <img
+                            className='image_dev'
+                            src={imagem_about}
+                            alt="Imagem de desenvolvedor"
+                        >
+                        </img>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card-body">
                             
+                            <p className="card-text text-start"> <span>Olá mundo! Sou o Rennan. </span> 
+                                Graduado em Análise e Desenvolvimento de Sistemas, curso em parceria com o Porto Digital,
+                                pela Universidade Tiradentes/PE. E estagiário na Autobem Brasil como desenvolvedor Front-End.
+                                Desta forma, me dedico a aprofundar os estudos em JavaScript com o framework Angular. Além de, possuir conhecimentos em Java e SpringBoot,
+                                para a construção de aplicações web.
+                            </p>
                         </div>
                     </div>
-                    
-                </div>
-                <div className="button">
-                    <button className="btn-download" data-back="Clique Aqui" data-front="Download CV">
-                    </button>
                 </div>
             </div>
-        
         </section>
-        
-
     )
 }
